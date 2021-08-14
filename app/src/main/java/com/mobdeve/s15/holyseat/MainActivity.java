5f8edd9c6429d425df85882c663ed6dcf93535c5
geosefuy <geosef_viktor_uy@dlsu.edu.ph>
2021-08-14 15:04:40 +0800

    removal of navigationviews

--- app/src/main/java/com/mobdeve/s15/holyseat/MainActivity.java
+++ app/src/main/java/com/mobdeve/s15/holyseat/MainActivity.java
@@ -18,46 +18,14 @@ import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
 
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_main);
-//
-//        BottomNavigationView bottomNavigationView = findViewById(R.id.Navbar);
-//        NavController navController = Navigation.findNavController(this,  R.id.FragmentContainer);
-//        NavigationUI.setupWithNavController(bottomNavigationView, navController);
-//    }
 
 
     private TextView title1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2);
+        setContentView(R.layout.activity_main);
 
-        title1 = findViewById(R.id.title1);
-        title1.setText("This is MainActivity");
-        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavbar);
-        BottomNavigationViewHelper.disableShiftMode(bottomNavigationView);
-        Menu menu = bottomNavigationView.getMenu();
-        MenuItem menuItem = menu.getItem(0);
-        menuItem.setChecked(true);
-
-        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                switch(item.getItemId()){
-                case R.id.menuHome:
-                    break;
-                case R.id.menuProfile:
-                    Intent intent = new Intent(MainActivity.this, ProfileActivity.class);
-                    startActivity(intent);
-                    MainActivity.this.overridePendingTransition(0, 0);
-                    break;
-                }
-                return false;
-            }
-        });
     }
 
 }
--- app/src/main/java/com/mobdeve/s15/holyseat/ProfileActivity.java
+++ app/src/main/java/com/mobdeve/s15/holyseat/ProfileActivity.java
@@ -22,29 +22,7 @@ public class ProfileActivity extends AppCompatActivity {
         setContentView(R.layout.activity_profile);
 
 
-        title2 = findViewById(R.id.title2);
-        title2.setText("This is ProfileActivity");
-        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavbar);
-        BottomNavigationViewHelper.disableShiftMode(bottomNavigationView);
-        Menu menu = bottomNavigationView.getMenu();
-        MenuItem menuItem = menu.getItem(1);
-        menuItem.setChecked(true);
 
-        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                switch(item.getItemId()){
-                case R.id.menuHome:
-                    Intent intent = new Intent(ProfileActivity.this, MainActivity.class);
-                    startActivity(intent);
-                    ProfileActivity.this.overridePendingTransition(0, 0);
-                    break;
-                case R.id.menuProfile:
-                    break;
-                }
-                return false;
-            }
-        });
     }
 
 }
